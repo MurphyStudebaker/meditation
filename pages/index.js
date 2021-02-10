8b7e1add40851b4bc9d1a67cb266046e8e7dc7aa
Murphy Studebaker <murphy.stude@gmail.com>
2021-02-09 20:58:06 -0800

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,123 @@
 import Head from 'next/head'
+import React, { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [duration, setDuration] = useState(1)
+  const [ minutesLeft, setMinutes ] = useState(0);
+  const [ secondsLeft, setSeconds ] =  useState(0);
+  const [ meditating, setMeditating ] = useState(false)
+  const [ width, setWidth ]  = useState(200)
+  const [ height, setHeight ] = useState(200)
+  
+  useEffect(()=>{
+  let myInterval = setInterval(() => {
+          if (secondsLeft > 0) {
+              setSeconds(secondsLeft - 1);
+          }
+          if (secondsLeft === 0) {
+              if (minutesLeft === 0) {
+                  clearInterval(myInterval)
+                  setMeditating(false)
+              } else {
+                  setMinutes(minutesLeft - 1);
+                  setSeconds(59);
+              }
+          } 
+      }, 1000)
+      return ()=> {
+          clearInterval(myInterval);
+        };
+  });
+
+  useEffect(() => {
+    const p5 = require("p5")
+    let windowWidth = window.innerWidth - 200|| 300
+    let windowHeight = window.innerHeight - 200 || 300
+
+    // figure out how to stop the animation once it is done
+
+    if (meditating) {
+      let sketch = new p5( p => {
+        let W = windowWidth
+        let H = windowHeight
+        let kMax = 5; // maximal value for the parameter "k" of the blobs
+        let step = 0.01; // difference in time between two consecutive blobs
+        let n = 100; // total number of blobs
+        let radius = 0; // radius of the base circle
+        let inter = 0.0005; // difference of base radii of two consecutive blobs
+        let maxNoise = 400; // maximal value for the parameter "noisiness" for the blobs
+        
+        //let noiseProg = (x) => (x);
+        p.setup = () => {
+          p.createCanvas(W, H)
+          p.colorMode(p.HSB, 1)
+          p.angleMode(p.DEGREES)
+          p.noFill();
+          //noLoop();
+          kMax = p.random(0.6, 1.0)
+          p.noStroke()
+        }
+
+        function blob(size, xCenter, yCenter, k, t, noisiness) {
+          p.beginShape();
+          let angleStep = 360 / 10;
+          for (let theta = 0; theta <= 360 + 2 * angleStep; theta += angleStep) {
+            let r1, r2;
+            r1 = p.cos(theta)+1;
+            r2 = p.sin(theta)+1; // +1 because it has to be positive for the function noise
+            let r = size + p.noise(k * r1,  k * r2, t) * noisiness;
+            let x = xCenter + r * p.cos(theta);
+            let y = yCenter + r * p.sin(theta);
+            p.curveVertex(x, y);
+          }
+          p.endShape();
+        }
+
+        p.draw = () => {
+          p.background(0.6, 0.75, 0.25);
+          let t = p.frameCount/100;
+          for (let i = n; i > 0; i--) {
+            let alpha = 1 - (i / n);
+            p.fill((alpha/5 + 0.75)%1, 1, 1, alpha);
+            let size = radius + i * inter;
+            let k = kMax * p.sqrt(i/n);
+            let noisiness = maxNoise * (i / n);
+            blob(size, W/2, H/2, k, t - i * step, noisiness);
+          }
+        } 
+      })
+    }
+    }, [meditating])
+
+  const startTimer = () => {
+    setMinutes(duration)
+    setSeconds(0)
+    setMeditating(true)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Meditate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="dark-blue flex flex-col items-center justify-center flex-1 px-20 text-center">
+        { !meditating && 
+          <div>
+            <h1>I want to meditate for <span className='font-bold'>{duration}</span> minutes.</h1>
+            <button onClick={e => startTimer()}className='px-3 py-2 border rounded'>Breathe</button>
+          </div>
+        }
+        { meditating && 
+          <div>{minutesLeft}:{secondsLeft}</div>
+        }
+        </main>
+        <footer className='w-full flex flex-row justify-start'>
+          <p>Help Me Meditate</p>
+          <a href='http://www.buildwithpride.org'> Built with 🏳️‍🌈 </a>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        </footer>
     </div>
   )
 }
