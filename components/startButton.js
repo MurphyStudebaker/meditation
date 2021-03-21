39ecbf2c8776912849340602be1d2fe93dbd849e
Murphy Studebaker <murphy.stude@gmail.com>
2021-03-21 09:47:25 -0700

    removed background white flicker on start button

--- components/startButton.js
+++ components/startButton.js
@@ -1,7 +1,7 @@
 export default function Start ({ start }) {
     return (
         <button onClick={e => start()}
-            className='transition duration-700 ease-in hover:text-white py-4 text-gray-200 text-2xl animated fadeIn'>
+            className='transition bg-transparent duration-700 ease-in hover:text-white py-4 text-gray-200 text-2xl hover:border-b-4 animated fadeIn'>
             I'm ready to begin ➜
         </button>
     )
