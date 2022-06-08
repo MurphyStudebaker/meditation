51d0989a3a545ea8e24520c6cbf1f6425843a892
Murphy Studebaker <murphy.stude@gmail.com>
2022-06-07 17:18:05 -0700

    styled start button

--- components/startButton.js
+++ components/startButton.js
@@ -1,10 +1,16 @@
 import Link from "next/link"
-
+import styled from "styled-components"
 export default function Start ({ start }) {
     return (
-        <Link href="/meditating">            
-            I'm ready to begin ➜
+        <Link href="/meditating" passHref>            
+            <StyledLink>I'm ready to begin ➜</StyledLink>
         </Link>
     )
 }
+
+const StyledLink = styled.a`
+    font-family: "Crimson Pro", sans-serif;
+    background-color: rgb(17,24,39);
+    padding: var(--spacing) calc(2*var(--spacing));
+`
 
